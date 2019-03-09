efef4571732e8211b4491b524ae8899d2ca3bbb2
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-03-09 20:52:38 +0100

    Add support for filtering ansi escapes

---
+++ .dir-locals.el
@@ -0,0 +1,1 @@
+((nil . ((projectile-project-compilation-cmd . "source ~/dlang/dmd-2.085.0/activate.fish && dub test -- -s -t -d"))))
--- source/colored/package.d
+++ source/colored/package.d
@@ -87,6 +87,31 @@ struct StyledString
     }
 }
 
+struct RGBString
+{
+    string unformatted;
+    ubyte r = 0xff;
+    ubyte g = 0xff;
+    ubyte b = 0xff;
+    this(string unformatted)
+    {
+        this.unformatted = unformatted;
+    }
+
+    auto rgb(ubyte r, ubyte g, ubyte b)
+    {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        return this;
+    }
+
+    string toString() @safe
+    {
+        return "\033[38;2;%s;%s;%sm%s\033[0m".format(r, g, b, unformatted);
+    }
+}
+
 @("styledstring") unittest
 {
     import unit_threaded;
@@ -195,6 +220,160 @@ ulong unformattedLength(string s)
         }
     }
     return count;
+}
+
+auto tokenize(Range)(Range parts)
+{
+    import std.range;
+
+    struct TokenizeResult(Range)
+    {
+        Range parts;
+        ElementType!(Range)[] next;
+        this(Range parts)
+        {
+            this.parts = parts;
+            tokenizeNext();
+        }
+
+        private void tokenizeNext()
+        {
+            next = [];
+            if (parts.empty)
+            {
+                return;
+            }
+            switch (parts.front)
+            {
+            case 38:
+            case 48:
+                next ~= 38;
+                parts.popFront;
+                switch (parts.front)
+                {
+                case 2:
+                    next ~= 2;
+                    parts.popFront;
+                    next ~= parts.front;
+                    parts.popFront;
+                    next ~= parts.front;
+                    parts.popFront;
+                    next ~= parts.front;
+                    parts.popFront;
+                    break;
+                case 5:
+                    next ~= 5;
+                    parts.popFront;
+                    next ~= parts.front;
+                    parts.popFront;
+                    break;
+                default:
+                    throw new Exception("Only [38,48];[2,5] are supported but got %s;%s".format(next[0],
+                            parts.front));
+                }
+                break;
+            case 0: .. case 37:
+            case 39: .. case 47:
+            case 49:
+            case 51: .. case 55:
+            case 60: .. case 65:
+            case 90: .. case 97:
+            case 100: .. case 107:
+                next ~= parts.front;
+                parts.popFront;
+                break;
+            default:
+                throw new Exception("Only colors are supported");
+            }
+        }
+
+        auto front()
+        {
+            return next;
+        }
+
+        bool empty()
+        {
+            return next == null;
+        }
+
+        void popFront()
+        {
+            tokenizeNext();
+        }
+    }
+
+    return TokenizeResult!(Range)(parts);
+}
+
+@("ansi tokenizer") unittest
+{
+    import unit_threaded;
+
+    [38, 5, 2, 38, 2, 1, 2, 3, 36, 1, 2, 3, 4].tokenize.shouldEqual([[38, 5,
+            2], [38, 2, 1, 2, 3], [36], [1], [2], [3], [4]]);
+}
+
+string filterAnsiEscapes(alias predicate)(string s)
+{
+    import std.regex;
+
+    string withFilters(Captures!string c)
+    {
+        import std.string;
+        import std.algorithm;
+        import std.conv;
+        import std.array;
+
+        auto parts = c[1].split(";").map!(a => a.to!uint).tokenize.filter!(p => predicate(p));
+        if (parts.empty)
+        {
+            return "";
+        }
+        else
+        {
+            return "\033[" ~ parts.joiner.map!(a => "%d".format(a)).join(";") ~ "m";
+        }
+    }
+
+    alias r = ctRegex!"\033\\[(.*?)m";
+    return s.replaceAll!(withFilters)(r);
+}
+
+bool foregroundColor(uint[] token)
+{
+    return token[0] >= 30 && token[0] <= 38;
+}
+
+bool backgroundColor(uint[] token)
+{
+    return token[0] >= 40 && token[0] <= 48;
+}
+
+bool style(uint[] token)
+{
+    return token[0] >= 1 && token[0] <= 29;
+}
+
+bool none(uint[] token)
+{
+    return false;
+}
+
+bool all(uint[] token)
+{
+    return true;
+}
+
+@("configurable strip") unittest
+{
+    import unit_threaded;
+    import std.functional;
+
+    "\033[1;31mtest".filterAnsiEscapes!(foregroundColor).shouldEqual("\033[31mtest");
+    "\033[1;31mtest".filterAnsiEscapes!(not!foregroundColor).shouldEqual("\033[1mtest");
+    "\033[1;31mtest".filterAnsiEscapes!(style).shouldEqual("\033[1mtest");
+    "\033[1;31mtest".filterAnsiEscapes!(none).shouldEqual("test");
 }
 
 auto leftJustifyFormattedString(string s, ulong width, dchar fillChar = ' ')
